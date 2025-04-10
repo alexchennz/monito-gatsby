9d737173e55c00f272cc1999eb283a6d82234abc
Alex Chen <newzealandbuy@gmail.com>
2025-04-10 17:32:04 +1200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `monito`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "iqUYJTZ-08qgoq815TSUbew0oTF0UbAixfBlHSjTvdQ",
+      "spaceId": "nwmkorofqn98"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
